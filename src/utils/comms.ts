91899be42599fd4da62b74e92802b6e96a473c3d
Alek Angelov <alek.angelov.sk@gmail.com>
2021-05-31 16:38:50 +0200

    fixing some commits and adding icns

--- src/utils/comms.ts
+++ src/utils/comms.ts
@@ -13,10 +13,14 @@ const toastListener = (event: any, data: any) => toast(data);
 
 export const notificationService = () => {
   ipcRenderer.on("notification", toastListener);
-  () => ipcRenderer.off("notification", toastListener);
+  return () => {
+    ipcRenderer.off("notification", toastListener);
+  };
 };
 
 export const fileOpen = (cb: (reply: any) => any) => {
   ipcRenderer.on("get-opened-files", cb);
-  () => ipcRenderer.off("notification", cb);
+  return () => {
+    ipcRenderer.off("get-opened-files", cb);
+  };
 };
