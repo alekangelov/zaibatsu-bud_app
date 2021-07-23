5c360419dbda05d1257746ddc24723b329b70b58
Alek Angelov <alek.angelov.sk@gmail.com>
2021-07-23 12:02:09 +0200

    Fixing character persistence

--- src/global/store.ts
+++ src/global/store.ts
@@ -4,6 +4,7 @@ import { characterReducer, comboReducer } from "./reducers";
 import electronStore from "./storageHelpers/electronStore";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { IState } from "./reducers/mainReducerTypes";
+import characters from "../data/characters";
 
 interface MigrationState extends IState {
   _persist: PersistState;
@@ -14,8 +15,10 @@ const persistConfig = {
   key: "root",
   version: 2,
   migrate: (state: MigrationState) => {
+    console.log({ state });
     return Promise.resolve({
       ...state,
+      characters: characters,
       combos: [...state.combos],
     });
   },
